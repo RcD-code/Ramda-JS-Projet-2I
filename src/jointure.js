1ea264bfff295feb99f61f40fcfac11354b11817
Raphaël Crueyze <raphaelcrueyze@MacBook-Air-de-Raphael-3.local>
2025-05-10 14:48:15 +0200

    Mise à jour du fichier jointure.js

--- src/jointure.js
+++ src/jointure.js
@@ -11,10 +11,15 @@ const totalMeasurements = R.length(measurements['records']);
 const totalSpecimens = R.length(specimens['records']);
 const totalOccurences = R.length(occurences['records']);
 
+const groupedBySpecies = R.groupBy(R.prop('tna'), specimens['records']);
+const countBySpecies = R.map(R.length, groupedBySpecies);
+
+
 const main = () => {
     console.log(`\nNombre total de mesures sur les dinosaures : ${totalMeasurements}`);
-    console.log(`\nNombre total d'occurences enregistré : ${totalOccurences}`);
-    console.log(`\nNombre total de specimens de dinosaures enregistré : ${totalSpecimens}`);
+    console.log(`\nNombre total d'occurrences enregistrées : ${totalOccurences}`);
+    console.log(`\nNombre total de parties du corps enregistrées : ${totalSpecimens}`);
+    console.log(`\nNombre d'os retrouvés par espèce : ${countBySpecies}`);
 };
 
 main();
